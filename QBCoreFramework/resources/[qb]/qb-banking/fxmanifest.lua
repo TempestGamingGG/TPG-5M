d7f46ba25058fe351b97353cf9a729685b712f2e
Joey Chmielewski <joey.chm@comcast.net>
2023-01-25 00:53:25 -0800

    ss

--- QBCoreFramework/resources/[qb]/qb-banking/fxmanifest.lua
+++ QBCoreFramework/resources/[qb]/qb-banking/fxmanifest.lua
@@ -2,14 +2,17 @@ fx_version 'cerulean'
 games { 'gta5' }
 
 shared_scripts {
+	'config.lua',
     'lua/shared/sh_*.lua',
 }
 
 client_scripts {
+	'config.lua',
     'lua/client/*.lua'
 }
 
 server_scripts {
+	'config.lua',
     '@oxmysql/lib/MySQL.lua',
     'lua/server/*.lua',
 }
